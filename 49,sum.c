25fdb7265ce6f2071a48353beaa9c5a7730852f4
786shankar <35800394+786shankar@users.noreply.github.com>
2018-03-16 03:15:47 -0700

    Create 49,sum.c

---
+++ 49,sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("enter the first value : ");
+    scanf("%d",&a);
+     printf("\nenter the second value : ");
+     scanf("%d",&b);
+     printf("\nthe sum of given two numbers is : %d",a+b);
+}
