8ca67f5ff58ea5b7a8ccc0662ea4c883e7569fd8
786shankar <35800394+786shankar@users.noreply.github.com>
2018-03-20 15:49:03 +0530

    Update 42,compare.c

--- 42,compare.c
+++ 42,compare.c
@@ -1,45 +1,26 @@
 #include<stdio.h>
-#include<string.h>
 void main()
 {
-    char a[100],b[100],c,d;
-    int i,j;
-    printf("enter the first string : ");
-    scanf("%s",a);
-     printf("\nenter the second string: ");
-     scanf("%s",b);
-     for(i=0,j=0;a[i]!='\0'||b[j]!='\0';i++,j++)
-     {
-      if(a[i]==b[j])
-      {
-          c=1;
-      }
-      else
-      {
-       c=2;   
-      }
-     }
-     if(c==1)
-     {
-     printf("the strings are equal %s",a);
-     }
-     else if(c==2)
-     printf("the given strings are not equal");
-     for(i=0;a[i]!='\0';i++)
-     {
-         
-     }
-     for(j=0;b[j]!='\0';j++)
-     {
-         
-     }
-     if(c==2)
-     {
-     if(i>j)
-     printf("\n%s",a);
-     else if(j>i)
-     printf("\n%s",b);
-     else 
-     printf("\n%s",b);
-     }
+    char a[100],b[100];
+    int i,val1,val2;
+    printf("enter the string 1: ");
+    gets(a);
+    printf("enter the string 2: ");
+    gets(b);
+    for(i=0;i<='\0';i++)
+    {
+        val1=val1+a[i];
+    }
+    for(i=0;i<='\0';i++)
+    {
+        val2=val2+b[i];
+    }
+    if(val1>val2)
+    {
+        printf("%s",a);
+    }
+    else
+    {
+        printf("%s",b);
+    }
 }
